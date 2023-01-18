027d01d40a821dfe9c110ad455b71abc52040411
xShiba <121438062+xShibaZ@users.noreply.github.com>
2023-01-18 19:58:51 +0700

    Update SaladFreeScript.lua

--- SaladFreeScript.lua
+++ SaladFreeScript.lua
@@ -1,4 +1,4 @@
-print("scriptisnotwork")
+print("scriptiswork")
  print("Welcome Salad X Hub  Free Script ")
  local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
  local Window  = Library.CreateLib("Salad X Hub", "BloodTheme")
